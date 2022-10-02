fda110720db788604a93740ddfffa0eade942118
Alex Farrant <65365105+FarrantAlex@users.noreply.github.com>
2022-10-02 11:13:23 +0100

    chmod -R 755 files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -248,7 +248,7 @@ do
 	fi
 done
 
-$DOCKER_COMPOSE exec tak bash -c "cd /opt/tak/certs && chown -R $USER:$USER *"
+$DOCKER_COMPOSE exec tak bash -c "chmod -R 755 /opt/tak/certs/files"
 
 printf $info "Waiting for TAK server to go live. This should take < 30s with an AMD64, ~1min on a ARM64 (Pi)\n"
 $DOCKER_COMPOSE start tak

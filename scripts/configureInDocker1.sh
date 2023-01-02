d52a306bf696dd187fdbbfb61f1fd2ea5ec88b82
Alex <alex@cloudrf.com>
2023-01-02 15:57:57 +0000

    Updated for 4.8 REL 31

--- scripts/configureInDocker1.sh
+++ scripts/configureInDocker1.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Added for 4.7 REL 18 where they broke DB auth
-#sed -i 's/127.0.0.1\/32/0.0.0.0\/0/g' /opt/tak/db-utils/pg_hba.conf
-# Removed inline options because these belong in postgres.conf
+# Added for 4.7 REL 18 where they broke DB auth with TCP/IP hardening
+# Commented out when they relaxed it in REL 4.7 20 because folks docker systems stopped working..
+# Re-added for 4.8 REL 31 because they got hard again. I can do this all day.
+sed -i 's/127.0.0.1\/32/0.0.0.0\/0/g' /opt/tak/db-utils/pg_hba.conf
 
+# Removed inline options because these belong in postgres.conf
 if [ -f "/var/lib/postgresql/data/postgresql.conf" ];
 then
 	echo "-------DB Exists-------"
